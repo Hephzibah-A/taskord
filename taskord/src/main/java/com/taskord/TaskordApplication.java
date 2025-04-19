1757fba481562beffcac1f0da12bbd53d81c3280
Hephzibah A <hephzibahantony11@gmail.com>
2025-04-19 11:07:05 +0530

    Initial commit

---
+++ taskord/src/main/java/com/taskord/TaskordApplication.java
@@ -0,0 +1,13 @@
+package com.taskord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskordApplication.class, args);
+	}
+
+}
---
+++ taskord/src/test/java/com/taskord/TaskordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
